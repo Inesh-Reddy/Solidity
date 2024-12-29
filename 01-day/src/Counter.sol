a70b383d29df9e375946f71b71de8db7a6fbf1ca
Inesh-Reddy <ineshreddy007@gmail.com>
2024-12-29 14:22:34 +0530

    changes to Counter file

--- 01-day/src/Counter.sol
+++ 01-day/src/Counter.sol
@@ -3,11 +3,9 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
-
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
-
     function increment() public {
         number++;
     }
