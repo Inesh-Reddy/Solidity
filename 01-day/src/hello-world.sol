d9f908e85bbde6669c39e3317a734d438edd2859
Inesh-Reddy <ineshreddy007@gmail.com>
2025-01-02 16:38:31 +0530

    added hello world contract

--- 01-day/src/hello-world.sol
+++ 01-day/src/hello-world.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.13;
 
 contract ZombieFactory {
     string public name;
+    
+    function setName(string memory _Name) public {
+        name = _Name;
+    }
     function getName() public view returns(string memory){
         return name;
     }
